cf4f019323012ddd1390c3501df7ba71be08b6e4
RomanRoman <przemekszewcu@interia.pl>
2017-11-23 11:12:33 +0100

    added unit test

--- stack.py
+++ stack.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+
+import unittest
+
 class Stack:
     def __init__(self):
         self.__data = []
@@ -15,28 +19,64 @@ class Stack:
         return self.__data.pop()
 
 
-stack = Stack()
-print("Ile liczb umiescic na stosie: ")
-d = input()
-d = int(d)
-for x in range(d):
-    try:
-        stack.push(input())
-    except Exception:
-        print("Bad input")
+#stack = Stack()
+#print("Ile liczb umiescic na stosie: ")
+#d = input()
+#d = int(d)
+#for x in range(d):
+#    try:
+#        stack.push(input())
+#    except Exception:
+#        print("Bad input")
+#
+#print(stack.empty())
+#print(stack.size())
+#print("ile liczb wyjac ze stosu: ")
+#d = input()
+#d = int(d)
+#for x in range(d):
+#    try:
+#        print(stack.pop())
+#    except Exception:
+#        print("Stos pusty")
 
-print(stack.empty())
-print(stack.size())
+#print("Na stosie zostalo liczb: ")
+#print(stack.size())
 
-print("ile liczb wyjac ze stosu: ")
-d = input()
-d = int(d)
-for x in range(d):
-    try:
-        print(stack.pop())
-    except Exception:
-        print("Stos pusty")
 
-print("Na stosie zostalo liczb: ")
+class TestStackMethods(unittest.TestCase):
 
-print(stack.size())
+    def test_empty(self):
+	s = Stack()
+        self.assertEqual(s.empty(), True)
+	s.push(2)
+	self.assertEqual(s.empty(), False)
+
+    def test_size(self):
+        s = Stack()
+        self.assertEqual(s.size(), 0)
+	s.push(2)
+	self.assertEqual(s.size(), 1)
+	s.push(3)
+	self.assertEqual(s.size(), 2)
+
+    def test_push(self):
+        s = Stack()
+	s.push(2)
+	s.push(3)
+	s.push(4)
+	self.assertEqual(s.pop(), 4)
+	self.assertEqual(s.pop(), 3)
+	self.assertEqual(s.pop(), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
